4a6d290997e79f17e249a064a0224ccd3f6ae428
Steven Schattenberg <steven.schattenberg@one.verizon.com>
2016-10-25 15:47:41 -0400

    Cleaned up code, added sendResponse function

--- traffic-cop.go
+++ traffic-cop.go
@@ -22,11 +22,13 @@ type tc struct {
 	Location string
 }
 
-type platformLocation struct {
-	Legacy string
-	OEM string
-}
-
+// The route handler function for the GET route for /svc/v1/traffic-cop.
+// This function will extract the userId parameter value out of the request, then
+// it will attempt to find a matching record in Redis. If the record is found
+// it will pass that to the sendResponse function to return it to the user. If
+// the record is not found it will attempt to find the record in the Cassandra
+// database. If it is found in Cassandra it will return it, if not it will return
+// an HTTP 404 error.
 func getHandler(w http.ResponseWriter, r *http.Request, session *gocql.Session, redisClient *redis.Client) {
 
 	var platform string
@@ -35,54 +37,45 @@ func getHandler(w http.ResponseWriter, r *http.Request, session *gocql.Session, 
 	// get the userId from the request object
 	userId := r.URL.Query().Get("userId")
 
-	// Set some standard headers
-	w.Header().Set("Content-Type", "application/json")
-
 	// check for the value in the Redis cache first
 	redisVal, redisErr := redisClient.Get(userId).Result()
 	if redisErr != nil {
-		//log.Fatal(err)
-
-		log.Print(redisErr)
-		log.Print("redisVal1: " + redisVal)
 
 		// query Cassandra for data if not found in Redis
 		if err := session.Query("SELECT platform FROM users WHERE users = " + userId).Scan(&platform); err != nil {
+
+			// If it is not in Redis and not in Cassandra return a 404
 			http.Error(w, err.Error(), http.StatusNotFound)
 			return
-		}
 
-		m := tc{platform,"http://www.foo.com"}
-		b, err := json.Marshal(m)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		} else {
-			w.Header().Set("X-Data-Origin", "Cassandra")
-			w.Write(b)
-			return
 		}
+		sendResponse(w, platform, "Cassandra")
 
 	// Return the values from Redis
 	} else {
-		log.Print(redisErr)
-		log.Print("redisVal2: " + redisVal)
-
-		m := tc{redisVal,"http://www.foo.com"}
-		b, err := json.Marshal(m)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		} else {
-			w.Header().Set("X-Data-Origin", "Redis")
-			w.Write(b)
-			return
-		}
-
+		sendResponse(w, redisVal, "Redis")
 	}
 
 }
 
+
+// Sends the actual HTTP response for a non-healthcheck request.
+func sendResponse(w http.ResponseWriter, platform string, xOrigin string){
+	m := tc{platform,"http://www.foo.com"}
+	b, err := json.Marshal(m)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("X-Data-Origin", xOrigin)
+		w.Write(b)
+		return
+	}
+}
+
+// The healthcheck function.  Checks the status of Redis and Cassandra.  Returns
+// HTTP 200 if they are healthy, HTTP 500 if they are not.
 func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	m := healthCheckMsg{"0.0.1", "traffic-cop-go", "localhost", "connected", "connected"}
 	b, err := json.Marshal(m)
